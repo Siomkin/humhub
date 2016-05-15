fdfc4902c85278dfc1b4f04ce7402c6853f00f1c
Siomkin Alexander <darang@tut.by>
2016-05-15 18:28:12 +0300

    composer

--- protected/config/web.php
+++ protected/config/web.php
@@ -1,5 +1,18 @@
 <?php
 
-return [
-];
+if (YII_ENV_DEV) {
+// configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        'allowedIPs' => ['127.0.0.1', '192.168.10.1', '46.53.216.44', '::1']
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'allowedIPs' => ['127.0.0.1', '192.168.10.1', '::1']
+    ];
+}
+return $config;
 
